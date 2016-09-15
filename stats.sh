3dea14a61c1635cd140372de38d9dbef569b2fb3
Rafael Alvarez <ralvarez@fluid.la>
2016-09-15 11:01:54 -0500

    Reparar problemas de linting del script

--- stats.sh
+++ stats.sh
@@ -8,31 +8,31 @@ else
   login="$1"
 fi
 
-challenges()
+_challenges()
 {
   local file="$1"
   local ext="$2"
-  find challenges -iname $file$ext | wc -l
+  find challenges -iname "$file""$ext" | wc -l
 }
 
-articles()
+_articles()
 {
 
   local dir=articles/"$1"	
   if [ -d "$dir" ]; then
-    ls -l "$dir" | wc -l
+    find "$dir" | wc -l
   else
     echo 0
   fi
 }
 
 echo "$login ha enviado"
-echo "$(articles $login) articulos"
-echo "$(challenges $login '*') retos"
-echo "- $(challenges $login '.asc') esta(n) en palabras"
-echo "- $(challenges $login '.py') esta(n) en Python"
-echo "- $(challenges $login '.rb') esta(n) en Ruby"
-echo "- $(challenges $login '.js') esta(n) en JavaScript"
-echo "- $(challenges $login '.java') esta(n) en Java"
-echo "- $(challenges $login '.cs') esta(n) en C#"
-echo "- $(challenges $login '.clj') esta(n) en Clojure"
+echo "$(_articles "$login") articulos"
+echo "$(_challenges "$login" "*") retos"
+echo "- $(_challenges "$login" ".asc") esta(n) en palabras"
+echo "- $(_challenges "$login" ".py") esta(n) en Python"
+echo "- $(_challenges "$login" ".rb") esta(n) en Ruby"
+echo "- $(_challenges "$login" ".js") esta(n) en JavaScript"
+echo "- $(_challenges "$login" ".java") esta(n) en Java"
+echo "- $(_challenges "$login" ".cs") esta(n) en C#"
+echo "- $(_challenges "$login" ".clj") esta(n) en Clojure"
