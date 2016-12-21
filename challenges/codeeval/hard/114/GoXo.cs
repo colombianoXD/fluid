f01aec8948e9aad0ae4fc0970d6a0c2f982d5ddd
Alejandro Aguirre Soto <aguirre.soto.alejandro@gmail.com>
2016-12-21 18:11:34 -0500

    Solucion codeeval hard 114 cs

--- challenges/codeeval/hard/114/GoXo.cs
+++ challenges/codeeval/hard/114/GoXo.cs
@@ -6,7 +6,6 @@ using System.Text.RegularExpressions;
 
 class PackageProblem
 {
-    //
     static Dictionary<int, double> weight = new Dictionary<int, double>();
     static Dictionary<int, double> price = new Dictionary<int, double>();
     static ArrayList answer = new ArrayList();
@@ -21,118 +20,87 @@ class PackageProblem
         {
 
             string line = reader.ReadLine();
-            if (line.Contains("-"))
+
+            Regex pattern = new Regex("[$()]");
+            line = pattern.Replace(line, "");
+            double max_Price = 0.0;
+            string res = "-";
+            string[] parts = line.Split(' ');
+            max_weight = Convert.ToDouble(parts[0]);
+            parsePaks(parts, max_weight);
+            recurPaks(1,parts.Length -2);
+
+            foreach(string id in answer)
             {
-                Console.WriteLine(line);
-                continue;
-            }
+                double curr_Price = 0.0;
+                string item = id.Remove(id.Length - 1);
 
-                Regex pattern = new Regex("[$()]");
-                line = pattern.Replace(line, "");
-
-                double max_Price = 0.0;
-                string res = "-";
-                string[] parts = line.Split(' ');
-                max_weight = Convert.ToDouble(parts[0]);
-                int lenPaks = parts.Length -2;
-
-                parsePaks(parts, max_weight);
-                recurPaks(1,lenPaks);
-
-                answer.Remove(null);
-
-
-                foreach(string id in answer)
+                foreach (string c in item.Split(','))
                 {
-                    if (id == " ") continue;
-
-                    double curr_Price = 0.0;
-                    string item = id.Remove(id.Length - 1);
-                    //Console.WriteLine(id);
-                    foreach (string c in item.Split(','))
-                    {
-                        //Console.WriteLine(c);
-                        curr_Price += price[Int32.Parse(c)];
-                    }
-
-                   if (curr_Price == max_Price)
-                    {
-                        double item_Weight = 9999999.0;
-                        double res_Weight = 9999999.0;
-
-                        foreach (string c in item.Split(','))
-                        {
-                            item_Weight += weight[Int32.Parse(c)];
-                        }
-
-                        foreach (string c in item.Split(','))
-                        {
-                            res_Weight += weight[Int32.Parse(c)];
-                        }
-
-                        if (item_Weight < res_Weight) res = item;
-                    }
-
-                    if (curr_Price > max_Price)
-                    {
-                        max_Price = curr_Price;
-                        res = item;
-                    }
-
+                    curr_Price += price[Int32.Parse(c)];
                 }
 
-                //res = res.Trim();
-                Console.WriteLine(res);
+               if (curr_Price == max_Price)
+                {
 
-            //Console.WriteLine(line);
+                    double item_Weight= 0.0;
+                    double res_Weight = 0.0;
+
+                    foreach (string c in item.Split(','))
+                    {
+                        item_Weight += weight[Int32.Parse(c)];
+                    }
+
+                    foreach (string c in res.Split(','))
+                    {
+                        res_Weight += weight[Int32.Parse(c)];
+                    }
+
+                    if (item_Weight < res_Weight) res = item;
+                }
+
+                if (curr_Price > max_Price)
+                {
+                    max_Price = curr_Price;
+                    res = item;
+                }
+            }
+
+            Console.WriteLine(res);
+
             temp = "";
             weight.Clear();
             price.Clear();
             answer.Clear();
             max_weight = 0.0;
             curr_weight = 0.0;
-
         }
     }
 
     static void recurPaks(int k, int len){
 
-
         if (k > len) return;
 
         for (int i = k; k <= len; i++){
 
-             //Console.WriteLine("antes: "+i);
              if (i > len) return;
              if (weight.ContainsKey(i)) curr_weight += weight[i];
              else continue;
 
-            // Console.WriteLine("despues:"+i);
-            // Console.WriteLine("curr_weight antes = "+curr_weight);
-
              if (curr_weight < max_weight)
              {
-                //temp += ",";
-                temp += i.ToString()+ ",";
-               // Console.WriteLine(temp);
-                if (!answer.Contains(temp)) answer.Add(temp.Trim());
-                //Console.WriteLine("Recursion a continuacion");
-                recurPaks(++i,len);
-                //continue;
 
+                temp += i.ToString()+ ",";
+                if (!answer.Contains(temp)) answer.Add(temp);
+                recurPaks(++i,len);
 
              }else{
                 curr_weight -= weight[i];
-                //Console.WriteLine("curr_weight despues = "+curr_weight);
                 continue;
              }
-             //Console.WriteLine("llego hastaaaaaaaaaa acaaaaaaa");
              i--;
-             //temp = temp.Remove(temp.Length - 1);
-
              temp = temp.Remove(temp.Length - i.ToString().Length - 1);
              curr_weight -= weight[i];
-
         }
         return;
     }
