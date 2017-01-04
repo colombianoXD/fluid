d1a96e9549b26d4859a55784b867d923666588fa
pach092 <pachobernal5@gmail.com>
2017-01-04 03:12:06 -0500

    Update and rename Pach092.py to pach092.py

--- challenges/codeabbey/004/Pach092.py
+++
@@ -1,7 +0,0 @@
-lista = [[5, 3],[2, 8],[100, 15]]
-total = []
-for a in lista:
-        valor = min(a)
-        total.append(str(valor))      
-for a in total:
-        print 'El numero menor es:',a
