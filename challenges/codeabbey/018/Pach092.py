e019bfb00d5300784f9ff3da8abc6004effd8ae1
pach092 <pachobernal5@gmail.com>
2017-01-04 01:36:46 -0500

    Update Pach092.py

--- challenges/codeabbey/018/Pach092.py
+++ challenges/codeabbey/018/Pach092.py
@@ -1,14 +1,12 @@
-datos = [150, 0, 5, 1, 10, 3]
-cantidad = len(datos)
-z = 0
+datos = int(raw_input())
 respuesta = []
-while z < cantidad:
-	a , b = int(datos[z]), int(datos[z + 1])
-	r = 1.00
-	for ecuaciones in range(b):
-		d = a / r
-		abs(r - d)
-		r = (r + d)/2
-	respuesta.append(str(r))
-	print(' '.join(respuesta))
-	z += 2
+for raiz in range(datos):
+    valores = map(int, raw_input().split())
+    a , b = int(valores[0]), int(valores[1])
+    r = 1.00
+    for ecuaciones in range(b):
+        d = a / r
+        abs(r - d)
+        r = (r + d)/2
+    respuesta.append(str(r))
+print(' '.join(respuesta))
