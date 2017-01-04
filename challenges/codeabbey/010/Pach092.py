cfcd3c294eb847425f89036f15fdb57b7a91434f
pach092 <pachobernal5@gmail.com>
2017-01-04 02:47:03 -0500

    Update Pach092.py

--- challenges/codeabbey/010/Pach092.py
+++ challenges/codeabbey/010/Pach092.py
@@ -1,13 +1,11 @@
-datos = [0, 0, 1, 1, 1, 0, 0, 1]
-cantidad = len(datos)
-z = 0
+datos = int(raw_input())
 respuesta = []
-while z < cantidad:
-	a, b, c, d = int(datos[z]), int(datos[z + 1]), int(datos[z + 2]), int(datos[z + 3])
-	pendiente = (d - b) / (c - a)
-	m = int(pendiente)
-	intercepcion = (b - m * a)
-	g = int(intercepcion)
-	respuesta.append('({0} {1})'.format(m, g))
-	print(' '.join(respuesta))
-	z += 4
+for funcion in range(datos):
+    valores = map(int, raw_input().split())
+    a, b, c, d = int(valores[0]), int(valores[1]), int(valores[2]), int(valores[3])
+    pendiente = (d - b) / (c - a)
+    m = int(pendiente)
+    intercepcion = (b - m * a)
+    g = int(intercepcion)
+    respuesta.append('({0} {1})'.format(m, g))
+print(' '.join(respuesta))
