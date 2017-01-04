f51823dc1071a303bb7ce2c06e8893482d225e19
pach092 <pachobernal5@gmail.com>
2017-01-03 21:08:50 -0500

    Create Pach092.py

---
+++ challenges/codeabbey/027/Pach092.py
@@ -0,0 +1,16 @@
+numeros = [3, 1, 4, 1, 5, 9, 2, 6]
+def orden(cadena, pasa, descartado):
+    respuesta = str(pasa), str(descartado) 
+    arreglado = True
+    pasa += 1
+    for a in range(len(cadena) - 1):
+        x = cadena[a]
+        y = cadena[a + 1]
+        if x > y:
+            arreglado = False
+            cadena[a], cadena[a + 1] = cadena[a + 1], cadena[a]
+            descartado += 1
+    if arreglado is False:
+        respuesta = orden(cadena, pasa, descartado)
+    return respuesta
+print ('%s %s') % orden(numeros, 1, 0)
