dc480d1652b1d9bc66473fe2dddad923ebabc54b
Rafael Alvarez <ralvarez@fluid.la>
2017-01-05 17:42:59 -0500

    Adicionando encabezados para log de CI

--- similarity-all.sh
+++ similarity-all.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo -e "\n###\n### Analizando similitud de programas.\n###\n"
+
 READY=$(egrep -R githubusercontent | cut -d":" -f1 | grep "codeeval\|codeabbey" | uniq | rev | cut -d"/" -f2- | rev | sort)
 
 MIN=${1:-5}
