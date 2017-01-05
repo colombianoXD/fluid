beb65e16535c87026bc07202cc3363937d3be2cb
Rafael Alvarez <ralvarez@fluid.la>
2017-01-05 18:53:54 -0500

    Soporte de multiples algortimos de similitud

--- similarity.sh
+++ similarity.sh
@@ -1,37 +1,11 @@
-#!/bin/bash 
+#!/bin/bash
 
-trap '' 2
+echo -e "\n###\n### Analizando similitud de programas.\n###\n"
 
-pushd . > /dev/null
+READY=$(egrep -R githubusercontent | cut -d":" -f1 | grep "codeeval\|codeabbey" | uniq | rev | cut -d"/" -f2- | rev | sort)
 
-CHALL="$1"
-cd "$CHALL"
+TYPE=${1:-typea}
 
-MIN=${2:-5}
-INDEX=$(for i in $(seq $MIN 9); do echo -n \\.$i\\\|;done; echo -n 1\\.0)
-
-OTHERS=$(cat OTHERS.txt)
-
-ID=0
-for other in $OTHERS; do
-  ID=$((ID + 1))
-  wget -T 4 -q $other -O $ID.other
+for ready in $READY; do 
+  ./similarity-"$TYPE".sh $ready
 done
-
-FILES=$(ls | grep -v '.txt')
-OWN=$(echo "$FILES" | grep -v '.other' | sort)
-MATRIX=$(simhash -f 256 -s 4 -m $FILES)
-HEADER=$(echo "$MATRIX" | head -1)
-HIGH=$(echo "$MATRIX" | grep $INDEX | grep -v '.other')
-
-if [ -n "$HIGH" ]; then
-   echo "Reto: $CHALL, Otros: $ID, Propios:" $OWN
-   echo "$HEADER"
-   echo "$HIGH"
-fi
-
-rm *.other
-
-popd > /dev/null
-
-trap 2
