4207852745cb8da33c93e3c783c490de58bbfc50
Luis Francisco Gutierrez Mendoza <luifrangutierrez@gmail.com>
2017-01-05 13:42:17 -0500

    Correciones
    Correciones lineas no permitidas

--- challenges/codeabbey/066/luifrangm.java
+++ challenges/codeabbey/066/luifrangm.java
@@ -1,21 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package pkg66.caesarciphercracker;
-
 import java.util.Scanner;
 
-/**
- *
- * @author LUIFRAN
- */
+
 public class CaesarCipherCracker {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
