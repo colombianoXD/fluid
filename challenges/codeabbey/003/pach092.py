7099044bbdffd86a909d944500257bb1edea8cbe
pach092 <pachobernal5@gmail.com>
2017-01-06 16:29:30 -0500

    Update pach092.py

--- challenges/codeabbey/003/pach092.py
+++ challenges/codeabbey/003/pach092.py
@@ -1,8 +1,6 @@
 datos = int(raw_input())
-respuesta = []
 for suma in range(datos):
-    valores = map(int, raw_input().split())
-    a, b = valores[0], valores[1]
+    valores = raw_input().split()
+    a, b = int(valores[0]), int(valores[1])
     res = a + b
-    respuesta.append(str(res))
-print (' '.join(respuesta))
+    print res,''
