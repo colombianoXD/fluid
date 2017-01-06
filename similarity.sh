f8d4d40ea48f0f839fc91ee658a83a3667fd1297
Rafael Alvarez <ralvarez@fluid.la>
2017-01-05 19:00:25 -0500

    Encabezado con tipo de algoritmo de similitud

--- similarity.sh
+++ similarity.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo -e "\n###\n### Analizando similitud de programas.\n###\n"
+TYPE=${1:-typea}
+
+echo -e "\n###\n### Analizando similitud ($TYPE) de programas.\n###\n"
 
 READY=$(egrep -R githubusercontent | cut -d":" -f1 | grep "codeeval\|codeabbey" | uniq | rev | cut -d"/" -f2- | rev | sort)
-
-TYPE=${1:-typea}
 
 for ready in $READY; do 
   ./similarity-"$TYPE".sh $ready
