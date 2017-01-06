50558fe193112f0a5ee666a96a3263115ee7e1e5
Rafael Alvarez <ralvarez@fluid.la>
2017-01-06 13:58:49 -0500

    Imprimir URL de build con folding section

---
+++ url.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "travis_fold:start:#url"
+echo "########################################################################"
+echo "########################################################################"
+echo "http://travisatfluid.s3-website-us-east-1.amazonaws.com/training/$TRAVIS_BUILD_NUMBER/$TRAVIS_JOB_NUMBER/people/"
+echo "########################################################################"
+echo "########################################################################"
+echo "travis_fold:end:#url"
