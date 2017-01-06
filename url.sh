62e7259d31c0a1bc08f09b433afd2e7419da23ad
Rafael Alvarez <ralvarez@fluid.la>
2017-01-06 14:08:09 -0500

    Imprimir URL al inicio del log

--- url.sh
+++ url.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "travis_fold:start:#url"
+echo -en "travis_fold:start:url\r"
 echo "########################################################################"
 echo "########################################################################"
 echo "http://travisatfluid.s3-website-us-east-1.amazonaws.com/training/$TRAVIS_BUILD_NUMBER/$TRAVIS_JOB_NUMBER/people/"
 echo "########################################################################"
 echo "########################################################################"
-echo "travis_fold:end:#url"
+echo -en "travis_fold:end:url\r"
