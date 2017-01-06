1523d526a910f770ca98a33bafee144da2bca9e8
pach092 <pachobernal5@gmail.com>
2017-01-06 17:15:22 -0500

    Update pach092.py

--- challenges/codeabbey/010/pach092.py
+++ challenges/codeabbey/010/pach092.py
@@ -1,5 +1,4 @@
 datos = int(raw_input())
-respuesta = []
 for funcion in range(datos):
     valores = map(int, raw_input().split())
     a, b, c, d = int(valores[0]), int(valores[1]), int(valores[2]), int(valores[3])
@@ -7,5 +6,4 @@ for funcion in range(datos):
     m = int(pendiente)
     intercepcion = (b - m * a)
     g = int(intercepcion)
-    respuesta.append('({0} {1})'.format(m, g))
-print(' '.join(respuesta))
+    print "(" + str(m), str(g) + ") "
