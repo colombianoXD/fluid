af6303abd69a29fae9eca2b87891569a1ba6f388
pach092 <pachobernal5@gmail.com>
2017-01-06 17:04:08 -0500

    Update pach092.py

--- challenges/codeabbey/008/pach092.py
+++ challenges/codeabbey/008/pach092.py
@@ -1,10 +1,8 @@
 datos = int(raw_input())
-respuesta = []
 for progresion in range(datos):
     valores = map(int, raw_input().split())
-    x, y, z = int(valores[0]), int(valores[1]), int(valores[2])
+    a, b, n = int(valores[0]), int(valores[1]), int(valores[2])
     total = 0
-    for b in range(z):
-        total = (x + (y * b)) + total
-    respuesta.append(str(total))
-print (' '.join(respuesta))
+    for x in range(n):
+       total = (a + (b * x)) + total
+    print total, ''
