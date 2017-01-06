575fa906fe7acd0ccb7c88289750d83547d5d453
pach092 <pachobernal5@gmail.com>
2017-01-06 16:37:38 -0500

    Update pach092.py

--- challenges/codeabbey/005/pach092.py
+++ challenges/codeabbey/005/pach092.py
@@ -1,8 +1,6 @@
 datos = int(raw_input())
-respuesta = []
 for minimo in range(datos):
-    valores = map(int, raw_input().split())
-    a, b, c = valores[0], valores[1], valores[2]
-    res = min(int(a), int(b), int(c))
-    respuesta.append(str(res))
-print (' '.join(respuesta))
+    valores = raw_input().split()
+    a, b, c = int(valores[0]), int(valores[1]), int(valores[2])
+    res = min(a, b, c)
+    print res, ''
