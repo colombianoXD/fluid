0cf2117c47c0b93064ac97fb67edb23275ea2989
b1nary <darkzxxz@gmail.com>
2017-01-06 16:33:31 -0500

    Update precommit.sh

--- precommit.sh
+++ precommit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git diff --name-only HEAD $(git merge-base HEAD master) | tee /dev/stdout | xargs pre-commit run --files
+echo $(git diff --name-only HEAD $(git merge-base HEAD master) | tee /dev/stdout | xargs pre-commit run --files)
