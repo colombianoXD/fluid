b39bde3d204aeda87409b305f9197c67890530cb
pach092 <pachobernal5@gmail.com>
2017-01-07 10:17:57 -0500

    Update pach092.py

--- challenges/codeabbey/010/pach092.py
+++ challenges/codeabbey/010/pach092.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-"""This script determines a and b by two points, belonging to a function."""
+"""This script determines a and b bytwo points, belonging to a function."""
 DATOS = int(raw_input())
 for funcion in range(DATOS):
     valores = map(int, raw_input().split())
-    a, b, c, d = int(valores[0]), int(valores[1]), int(valores[2]), int(valores[3])
+    a, b = int(valores[0]), int(valores[1])
+    c, d = int(valores[2]), int(valores[3])
     pendiente = (d - b) / (c - a)
     m = int(pendiente)
     intercepcion = (b - m * a)
