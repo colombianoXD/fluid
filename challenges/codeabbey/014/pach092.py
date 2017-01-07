db5a41e65f3787ff822bcea86cc5a2e98d8b19b6
pach092 <pachobernal5@gmail.com>
2017-01-07 00:05:52 -0500

    Update pach092.py

--- challenges/codeabbey/014/pach092.py
+++ challenges/codeabbey/014/pach092.py
@@ -1,13 +1,16 @@
-datos=eval(raw_input())
-while(1==1):    
-    cadena=raw_input().split()
-    operador=cadena[0]
-    numero=int(cadena[1])
-    if(operador=='%'):
-        ans=datos%numero
-        print(ans)
+#!/usr/bin/env python
+"""This script gives remainder of the result of all operations applied
+sequentially (starting with initial number) divided by the last number."""
+DATOS = int(raw_input())
+while 1 == 1:
+    CADENA = raw_input().split()
+    OPERADOR = CADENA[0]
+    NUMERO = int(CADENA[1])
+    if OPERADOR == '%':
+        RESPUESTA = DATOS % NUMERO
+        print RESPUESTA
         break
-    elif(operador=='*'):
-        datos=datos*numero
+    elif OPERADOR == '*':
+        DATOS = DATOS * NUMERO
     else:
-        datos=datos+numero
+        DATOS = DATOS + NUMERO
