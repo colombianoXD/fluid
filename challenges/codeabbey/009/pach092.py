4b0a7795c344e2455d8ab5ac3d37b44b0dce38eb
pach092 <pachobernal5@gmail.com>
2017-01-06 21:10:38 -0500

    Update pach092.py

--- challenges/codeabbey/009/pach092.py
+++ challenges/codeabbey/009/pach092.py
@@ -1,16 +1,16 @@
-datos = int(raw_input())
-for triangulos in range(datos):
-    valores = raw_input().split()
-    mayor = max(int(valores[0]),int(valores[1]),int(valores[2]))
+datos=int(raw_input())
+for(triangulos)in range(datos):
+    valores=raw_input().split()
+    mayor=max(int(valores[0]),int(valores[1]),int(valores[2]))
     valores.remove(str(mayor))
-    a, b = int(valores[0]), int(valores[1])
-    c = mayor
-    if (a + b) > c and (a + c) > b and (b + c) > a:
-        print 1 
-        print " "
-    elif (a + b) == c:
-        print 1
-        print " "
+    a,b=int(valores[0]),int(valores[1])
+    c=mayor
+    if((a+b)>c)and((a+c)>b)and(b+c)>a:
+        print(1)
+        print(" ")
+    elif(a+b)==c:
+        print(1)
+        print(" ")
     else:
-        print 0
-        print " "
+        print(0)
+        print(" ")
