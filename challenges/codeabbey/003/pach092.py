e08e0262367ecb16fa762bda7730efa007911be3
pach092 <pachobernal5@gmail.com>
2017-01-06 23:12:12 -0500

    Update pach092.py

--- challenges/codeabbey/003/pach092.py
+++ challenges/codeabbey/003/pach092.py
@@ -1,6 +1,8 @@
-datos=int(raw_input())
-for(suma)in range(datos):
-    valores=raw_input().split()
-    a,b=int(valores[0]),int(valores[1])
-    res=a+b
-    print(str(res)+' ')
+#!/usr/bin/env python
+"""This script sums the values of each string and returns the response of the sums"""
+DATOS = int(raw_input())
+for suma in range(DATOS):
+    valores = raw_input().split()
+    a, b = int(valores[0]), int(valores[1])
+    res = a + b
+    print res, ' '
