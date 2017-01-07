2c96f79d8d4e60826217a71aa91f8671787418c1
pach092 <pachobernal5@gmail.com>
2017-01-07 00:08:00 -0500

    Update pach092.py

--- challenges/codeabbey/019/pach092.py
+++ challenges/codeabbey/019/pach092.py
@@ -1,20 +1,24 @@
-abren = ['(' , '[' , '{' , '<']
-cierran = [')' , ']' , '}' , '>']
-datos = int(raw_input())
-for a in range (datos):
+#!/usr/bin/env python
+"""This script gives as answer 1 (if bracket order is correct) or
+0 (if incorrect) for each of test-cases, separated by spaces."""
+ABREN = ['(', '[', '{', '<']
+CIERRAN = [')', ']', '}', '>']
+DATOS = int(raw_input())
+for a in range(DATOS):
     cadena = list(raw_input())
     comprueba = []
     resultado = 1
     for i in cadena:
-        if i in abren:
+        if i in ABREN:
             comprueba.append(i)
-        elif i in cierran:
-            ind = cierran.index(i)
+        elif i in CIERRAN:
+            ind = CIERRAN.index(i)
             if comprueba == []:
                 resultado = 0
-            elif comprueba[-1] == abren[ind]:
+            elif comprueba[-1] == ABREN[ind]:
                 comprueba = comprueba[0:len(comprueba)-1]
             else:
                 resultado = 0
-    if comprueba != []: resultado = 0
+    if comprueba != []:
+        resultado = 0
     print resultado, ''
