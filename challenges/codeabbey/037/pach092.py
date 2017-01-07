7097d413902179d9034eccfbfa48b625c255ebca
pach092 <pachobernal5@gmail.com>
2017-01-06 20:16:40 -0500

    Update pach092.py

--- challenges/codeabbey/037/pach092.py
+++ challenges/codeabbey/037/pach092.py
@@ -1,8 +1,6 @@
 datos = raw_input().split()
 cantidad = len(datos)
 a = 0
-p = int(datos[0])
-i = (int(datos[1]) / 100.0) / 12
-m = int(datos[2])
+p, i, m = int(datos[0]), (int(datos[1]) / 100.0) / 12, int(datos[2])
 mensualidad = p*(i*(1+i)**m)/((1+i)**m-1)
 print(round(mensualidad))
