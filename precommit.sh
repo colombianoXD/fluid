47bed7c508480b058e9b00bd80495a4af5e002cd
Rafael Alvarez <ralvarez@fluid.la>
2017-01-06 20:47:47 -0500

    Imprimir archivos modificados uno por linea

--- precommit.sh
+++ precommit.sh
@@ -2,7 +2,7 @@
 
 CHANGED=$(git diff --name-only HEAD $(git merge-base HEAD master))
 
-echo "Archivos modificados:"
-echo $CHANGED
+echo "### Archivos modificados en este cambio"
+echo "$CHANGED"
 
 pre-commit run --files $CHANGED
