9afd25df36e2c7c625d745eae90dc35083bec254
pach092 <pachobernal5@gmail.com>
2017-01-07 00:05:26 -0500

    Update pach092.py

--- challenges/codeabbey/013/pach092.py
+++ challenges/codeabbey/013/pach092.py
@@ -1,8 +1,10 @@
-datos=int(raw_input())
-numeros=raw_input().split()
-for(a)in(numeros):
-    suma=0
-    for(a,n)in enumerate(a):
-        a+=1
-        suma+=int(n)*a
-    print(str(suma)+' ')
+#!/usr/bin/env python
+"""This script multiplies each digit by its position."""
+DATOS = int(raw_input())
+NUMEROS = raw_input().split()
+for a in NUMEROS:
+    suma = 0
+    for a, n in enumerate(a):
+        a += 1
+        suma += int(n) * a
+    print suma, ''
