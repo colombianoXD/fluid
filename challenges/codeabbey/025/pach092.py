572dff7f5746637c5fe92d8ddc0ef2a739cce228
pach092 <pachobernal5@gmail.com>
2017-01-07 00:53:11 -0500

    Update pach092.py

--- challenges/codeabbey/025/pach092.py
+++ challenges/codeabbey/025/pach092.py
@@ -1,7 +1,10 @@
-datos = int(input())
-for lineal in range(datos):
+#!/usr/bin/env python
+"""This script calculates N-th member's value for each test-case, separated by spaces."""
+DATOS = int(input())
+for lineal in range(DATOS):
     valores = raw_input().split()
-    a, c, m, x, n = int(valores[0]), int(valores[1]), int(valores[2]), int(valores[3]), int(valores[4])
+    a, c, m = int(valores[0]), int(valores[1]), int(valores[2])
+    x, n = int(valores[3]), int(valores[4])
     for resultado in range(n):
         x = (a * x + c) % m
     print x, ''
