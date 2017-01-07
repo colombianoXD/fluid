df2481d64006b443eabb706ab33f9561ca1b1f19
Rafael Alvarez <ralvarez@fluid.la>
2017-01-06 21:01:55 -0500

    Secciones dolbables con ancla

--- precommit.sh
+++ precommit.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo -en "travis_fold:start:#precommit\r"
+
 CHANGED=$(git diff --name-only HEAD $(git merge-base HEAD master))
 
 echo "### Archivos modificados en este cambio"
@@ -10,3 +12,5 @@ else
   echo "(Ninguno)"
   echo "Omitiendo ejecución de precommit."
 fi
+
+echo -en "travis_fold:end:#precommit\r"
