119fe9fca4025a8ca6669f2dc3abc4a61ff78d03
pach092 <pachobernal5@gmail.com>
2017-01-06 20:59:42 -0500

    Update pach092.py

--- challenges/codeabbey/003/pach092.py
+++ challenges/codeabbey/003/pach092.py
@@ -1,6 +1,6 @@
-datos = int(raw_input())
-for suma in range(datos):
-    valores = raw_input().split()
-    a, b = int(valores[0]), int(valores[1])
-    res = a + b
-    print res,''
+datos=int(raw_input())
+for(suma)in range(datos):
+    valores=raw_input().split()
+    a,b=int(valores[0]),int(valores[1])
+    res=a+b
+    print(str(res)+' ')
