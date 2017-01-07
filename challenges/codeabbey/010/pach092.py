81e8c820b5f1f0c757b2714333a8483e000d32b1
pach092 <pachobernal5@gmail.com>
2017-01-06 21:12:52 -0500

    Update pach092.py

--- challenges/codeabbey/010/pach092.py
+++ challenges/codeabbey/010/pach092.py
@@ -1,9 +1,9 @@
-datos = int(raw_input())
-for funcion in range(datos):
-    valores = map(int, raw_input().split())
-    a, b, c, d = int(valores[0]), int(valores[1]), int(valores[2]), int(valores[3])
-    pendiente = (d - b) / (c - a)
-    m = int(pendiente)
-    intercepcion = (b - m * a)
-    g = int(intercepcion)
-    print "(" + str(m), str(g) + ") "
+datos=int(raw_input())
+for(funcion)in range(datos):
+    valores=map(int,raw_input().split())
+    a,b,c,d=int(valores[0]),int(valores[1]),int(valores[2]),int(valores[3])
+    pendiente=(d-b)/(c-a)
+    m=int(pendiente)
+    intercepcion=(b-m*a)
+    g=int(intercepcion)
+    print "("+str(m),str(g)+") "
