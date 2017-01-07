669852faa892f363a636d1a33d621c998c9a022e
pach092 <pachobernal5@gmail.com>
2017-01-06 23:14:18 -0500

    Update pach092.py

--- challenges/codeabbey/008/pach092.py
+++ challenges/codeabbey/008/pach092.py
@@ -1,8 +1,10 @@
-datos=int(raw_input())
-for(progresion)in range(datos):
-    valores=map(int,raw_input().split())
-    a,b,n=int(valores[0]),int(valores[1]),int(valores[2])
-    total=0
-    for(x)in range(n):
-       total=(a+(b*x))+total
-    print(str(total)+' ')
+#!/usr/bin/env python
+"""This script calculates the sum of first members of arithmetic sequence."""
+DATOS = int(raw_input())
+for progresion in range(DATOS):
+    valores = map(int, raw_input().split())
+    a, b, n = int(valores[0]), int(valores[1]), int(valores[2])
+    total = 0
+    for x in range(n):
+        total = (a + (b * x)) + total
+    print total, ''
