545b7e7b1e1b937149f8ea73d883f2d5b8d8c20c
pach092 <pachobernal5@gmail.com>
2017-01-06 19:48:46 -0500

    Update pach092.py

--- challenges/codeabbey/025/pach092.py
+++ challenges/codeabbey/025/pach092.py
@@ -1,11 +1,7 @@
-contador = int(raw_input())
-respuesta = []
-for operaciones in range(contador):
-    valores = map(int, raw_input().split())
-    a, c, m, x0, n = int(valores[0]), int(valores[1]), int(valores[2]), int(valores[3]), int(valores[4])
-    x_pruebas = x0
-    for x in range (n):
-        x_siguiente = (a * x_pruebas + c) %m
-        x_pruebas = x_siguiente
-    respuesta.append(str(x_pruebas))
-print(' '.join(respuesta))
+datos = int(input())
+for lineal in range(datos):
+    valores = raw_input().split()
+    a, c, m, x, n = int(valores[0]), int(valores[1]), int(valores[2]), int(valores[3]), int(valores[4])
+    for resultado in range(n):
+        x = (a * x + c) % m
+    print x, ''
