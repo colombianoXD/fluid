0e072a7e018af48f930a3b4cc44bc48095ef325f
pach092 <pachobernal5@gmail.com>
2017-01-06 21:08:10 -0500

    Update pach092.py

--- challenges/codeabbey/008/pach092.py
+++ challenges/codeabbey/008/pach092.py
@@ -1,8 +1,8 @@
-datos = int(raw_input())
-for progresion in range(datos):
-    valores = map(int, raw_input().split())
-    a, b, n = int(valores[0]), int(valores[1]), int(valores[2])
-    total = 0
-    for x in range(n):
-       total = (a + (b * x)) + total
-    print total, ''
+datos=int(raw_input())
+for(progresion)in range(datos):
+    valores=map(int,raw_input().split())
+    a,b,n=int(valores[0]),int(valores[1]),int(valores[2])
+    total=0
+    for(x)in range(n):
+       total=(a+(b*x))+total
+    print(str(total)+' ')
