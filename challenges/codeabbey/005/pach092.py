c59624613b268a505b1ccff65dee54ea8b8bef3e
pach092 <pachobernal5@gmail.com>
2017-01-06 21:03:50 -0500

    Update pach092.py

--- challenges/codeabbey/005/pach092.py
+++ challenges/codeabbey/005/pach092.py
@@ -1,6 +1,6 @@
-datos = int(raw_input())
-for minimo in range(datos):
-    valores = raw_input().split()
-    a, b, c = int(valores[0]), int(valores[1]), int(valores[2])
-    res = min(a, b, c)
-    print res, ''
+datos=int(raw_input())
+for(minimo)in range(datos):
+    valores=raw_input().split()
+    a,b=int(valores[0]),int(valores[1])
+    res=min(a,b)
+    print(str(res)+' ')
