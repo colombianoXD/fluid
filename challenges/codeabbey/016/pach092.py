3aa8621fff92a6a71a7a83ef97d7c8ad2b64dc05
pach092 <pachobernal5@gmail.com>
2017-01-07 10:56:48 -0500

    Update pach092.py

--- challenges/codeabbey/016/pach092.py
+++ challenges/codeabbey/016/pach092.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-"""This script gives the average values for each array, rounded to nearest integer."""
+"""This script gives the average values for
+each array, rounded to nearest integer."""
 DATOS = int(raw_input())
 for linea in range(DATOS):
     numeros = map(int, raw_input().split())[:-1]
