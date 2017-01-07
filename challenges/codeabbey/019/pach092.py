7cecc0356fb15d132254dc1bea12b27ee5987bf6
pach092 <pachobernal5@gmail.com>
2017-01-06 19:26:33 -0500

    Update pach092.py

--- challenges/codeabbey/019/pach092.py
+++ challenges/codeabbey/019/pach092.py
@@ -1,22 +1,20 @@
 abren = ['(' , '[' , '{' , '<']
 cierran = [')' , ']' , '}' , '>']
 datos = int(raw_input())
-respuesta = []
-for a in range (datos) :
+for a in range (datos):
     cadena = list(raw_input())
     comprueba = []
     resultado = 1
-    for i in cadena :
-        if i in abren :
+    for i in cadena:
+        if i in abren:
             comprueba.append(i)
-        elif i in cierran :
+        elif i in cierran:
             ind = cierran.index(i)
-            if comprueba == [] :
+            if comprueba == []:
                 resultado = 0
-            elif comprueba[-1] == abren[ind] :
+            elif comprueba[-1] == abren[ind]:
                 comprueba = comprueba[0:len(comprueba)-1]
-            else :
+            else:
                 resultado = 0
-    if comprueba != [] : resultado = 0
-    respuesta.append(str(resultado))
-print(' '.join(respuesta))
+    if comprueba != []: resultado = 0
+    print resultado, ''
