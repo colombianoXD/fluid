72ef0d6acbd035f3f2569fdf4a5f88fab94f0b7f
pach092 <pachobernal5@gmail.com>
2017-01-06 21:14:53 -0500

    Update pach092.py

--- challenges/codeabbey/011/pach092.py
+++ challenges/codeabbey/011/pach092.py
@@ -1,10 +1,9 @@
-datos = int(raw_input())
-for sumatoria in range(datos):
-    valores = raw_input().split()
-    a, b, c = int(valores[0]), int(valores[1]), int(valores[2])
-    total = ((a * b) + c)
-    total = list(str(total))
-    suma = 0
-    for resultado in total:
-        suma += int(resultado)
-    print suma, ''
+datos=int(raw_input())
+for(sumatoria)in range(datos):
+    valores=raw_input().split()
+    a,b,c=int(valores[0]),int(valores[1]),int(valores[2])
+    total=list(str(((a*b)+c)))
+    suma=0
+    for(resultado)in(total):
+        suma+=int(resultado)
+    print(str(suma)+' ')
