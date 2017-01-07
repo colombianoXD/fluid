e1c5eef7741f3f1a049e02cc01ba94ef15694315
pach092 <pachobernal5@gmail.com>
2017-01-06 21:18:43 -0500

    Update pach092.py

--- challenges/codeabbey/013/pach092.py
+++ challenges/codeabbey/013/pach092.py
@@ -1,8 +1,8 @@
-datos = int(raw_input())
-numeros = raw_input().split()
-for a in numeros:
-    suma = 0
-    for (a, n) in enumerate(a):
-        a += 1
-        suma += int(n) * a
-    print suma, ''
+datos=int(raw_input())
+numeros=raw_input().split()
+for(a)in(numeros):
+    suma=0
+    for(a,n)in enumerate(a):
+        a+=1
+        suma+=int(n)*a
+    print(str(suma)+' ')
