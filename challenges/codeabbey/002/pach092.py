b3660c6839e89e13c74897c782c7aa2959afaab9
pach092 <pachobernal5@gmail.com>
2017-01-06 23:11:45 -0500

    Update pach092.py

--- challenges/codeabbey/002/pach092.py
+++ challenges/codeabbey/002/pach092.py
@@ -1,6 +1,8 @@
-datos=raw_input()
-valores=raw_input().split()
-suma=0
-for(sumatoria)in(valores):
-    suma+=int(sumatoria)
-print suma
+#!/usr/bin/env python
+"""This script adds a string and returns the answer"""
+DATOS = raw_input()
+VALORES = raw_input().split()
+SUMA = 0
+for sumatoria in VALORES:
+    SUMA += int(sumatoria)
+print SUMA
