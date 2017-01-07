d4f053224cd058318bdf513f8180f63865be5468
pach092 <pachobernal5@gmail.com>
2017-01-06 21:06:45 -0500

    Update pach092.py

--- challenges/codeabbey/007/pach092.py
+++ challenges/codeabbey/007/pach092.py
@@ -1,5 +1,5 @@
-celcius = raw_input().split()
-fahrenheit = 0
-for temperatura in celcius[1:]:
-    fahrenheit = int(round((float(temperatura)-32)*5/9))
-    print fahrenheit,''
+celcius=raw_input().split()
+fahrenheit=0
+for(temperatura)in celcius[1:]:
+    fahrenheit=int(round((float(temperatura)-32)*5/9))
+    print(str(fahrenheit)+' ')
