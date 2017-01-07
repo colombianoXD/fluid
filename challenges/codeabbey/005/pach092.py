18cf882985279cd84f8d26e2728411650b7bc521
pach092 <pachobernal5@gmail.com>
2017-01-07 09:32:36 -0500

    Update pach092.py

--- challenges/codeabbey/005/pach092.py
+++ challenges/codeabbey/005/pach092.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-"""This script looks for the minimum value of each string and returns the answer"""
+"""This script looks for the minimum value
+of each string and returns the answer"""
 DATOS = int(raw_input())
 for minimo in range(DATOS):
     valores = raw_input().split()
