d97e570dbbf47dc61e1910b48bea22e886588a36
pach092 <pachobernal5@gmail.com>
2017-01-06 21:05:33 -0500

    Update pach092.py

--- challenges/codeabbey/006/pach092.py
+++ challenges/codeabbey/006/pach092.py
@@ -1,7 +1,7 @@
-datos = int(raw_input())
-for redondeo in range(datos):
-    valores = raw_input().split()
-    a, b = float(valores[0]), float(valores[1])
-    res = a / b
-    respuesta = round(res)
-    print int(respuesta), ''
+datos=int(raw_input())
+for(redondeo)in range(datos):
+    valores=raw_input().split()
+    a,b=float(valores[0]),float(valores[1])
+    res=a/b
+    respuesta=round(res)
+    print(str(int(respuesta))+" ")
