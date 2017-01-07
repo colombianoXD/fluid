f847837abd64f67213e596d7ac8b96ef5671a4b9
pach092 <pachobernal5@gmail.com>
2017-01-07 00:04:41 -0500

    Update pach092.py

--- challenges/codeabbey/011/pach092.py
+++ challenges/codeabbey/011/pach092.py
@@ -1,9 +1,11 @@
-datos=int(raw_input())
-for(sumatoria)in range(datos):
-    valores=raw_input().split()
-    a,b,c=int(valores[0]),int(valores[1]),int(valores[2])
-    total=list(str(((a*b)+c)))
-    suma=0
-    for(resultado)in(total):
-        suma+=int(resultado)
-    print(str(suma)+' ')
+#!/usr/bin/env python
+"""This script calculates the sums of given digits."""
+DATOS = int(raw_input())
+for sumatoria in range(DATOS):
+    valores = raw_input().split()
+    a, b, c = int(valores[0]), int(valores[1]), int(valores[2])
+    total = list(str(((a * b) + c)))
+    suma = 0
+    for resultado in total:
+        suma += int(resultado)
+    print suma, ''
