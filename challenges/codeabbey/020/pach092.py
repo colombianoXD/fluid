c2e9ca0023c5780ff5f1163c37f33979e8a2924e
pach092 <pachobernal5@gmail.com>
2017-01-06 19:29:25 -0500

    Update pach092.py

--- challenges/codeabbey/020/pach092.py
+++ challenges/codeabbey/020/pach092.py
@@ -1,9 +1,7 @@
 datos = int(raw_input())
 vocales = list("aeiouy")
-respuesta = []
 for pasos in range(datos):
-    oraciones = map(str, raw_input().split("\n"))
+    oraciones = raw_input().split("\n")
     frase = oraciones[0].lower()
     contador_vocales = sum(frase.count(v) for v in vocales)
-    respuesta.append(str(contador_vocales))
-print(' '.join(respuesta))
+    print contador_vocales, ''
