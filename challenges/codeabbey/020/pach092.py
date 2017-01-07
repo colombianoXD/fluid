d19c1da661a8660185f9ef9e15ea498253369931
pach092 <pachobernal5@gmail.com>
2017-01-07 00:08:21 -0500

    Update pach092.py

--- challenges/codeabbey/020/pach092.py
+++ challenges/codeabbey/020/pach092.py
@@ -1,7 +1,9 @@
-datos = int(raw_input())
-vocales = list("aeiouy")
-for pasos in range(datos):
+#!/usr/bin/env python
+"""This script counts the number of vowels in each line, separated by spaces."""
+DATOS = int(raw_input())
+VOCALES = list("aeiouy")
+for pasos in range(DATOS):
     oraciones = raw_input().split("\n")
     frase = oraciones[0].lower()
-    contador_vocales = sum(frase.count(v) for v in vocales)
+    contador_vocales = sum(frase.count(v) for v in VOCALES)
     print contador_vocales, ''
