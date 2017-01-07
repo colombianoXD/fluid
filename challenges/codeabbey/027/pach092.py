8251fc7f50f10e00ff0c0a56547ed82f102519f7
pach092 <pachobernal5@gmail.com>
2017-01-07 10:31:46 -0500

    Update pach092.py

--- challenges/codeabbey/027/pach092.py
+++ challenges/codeabbey/027/pach092.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
-"""This script calculates number of passes perfromed and total number of swaps made. """
+"""This script calculates number of passes
+perfromed and total number of swaps made."""
 DATOS = int(raw_input())
 NUMEROS = [int(z) for z in raw_input().split()]
+
+
 def orden(cadena, pasa, descartado):
     """This def calculates the swaps and passes from the array"""
     respuesta = str(pasa), str(descartado)
@@ -12,9 +15,12 @@ def orden(cadena, pasa, descartado):
         ___y___ = cadena[___a___ + 1]
         if ___x___ > ___y___:
             arreglado = False
-            cadena[___a___], cadena[___a___ + 1] = cadena[___a___ + 1], cadena[___a___]
+            cadena[___a___], cadena[___a___ + 1] =\
+                cadena[___a___ + 1], cadena[___a___]
             descartado += 1
     if arreglado is False:
         respuesta = orden(cadena, pasa, descartado)
     return respuesta
+
+
 print ('%s %s') % orden(NUMEROS, 1, 0)
