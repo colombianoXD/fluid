d734f421514934e51764092246d56f06d48bb723
pach092 <pachobernal5@gmail.com>
2017-01-06 19:24:27 -0500

    Update pach092.py

--- challenges/codeabbey/018/pach092.py
+++ challenges/codeabbey/018/pach092.py
@@ -1,12 +1,9 @@
 datos = int(raw_input())
-respuesta = []
 for raiz in range(datos):
-    valores = map(int, raw_input().split())
-    a , b = int(valores[0]), int(valores[1])
-    r = 1.00
+    valores = raw_input().split()
+    a, b = int(valores[0]), int(valores[1])
+    r = float(1)
     for ecuaciones in range(b):
         d = a / r
-        abs(r - d)
         r = (r + d)/2
-    respuesta.append(str(r))
-print(' '.join(respuesta))
+    print r, ''
