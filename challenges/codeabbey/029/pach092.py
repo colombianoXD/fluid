7893925efd9330cef962b9b6e76ddf1dd4f1d859
pach092 <pachobernal5@gmail.com>
2017-01-07 00:54:32 -0500

    Update pach092.py

--- challenges/codeabbey/029/pach092.py
+++ challenges/codeabbey/029/pach092.py
@@ -1,23 +1,25 @@
-datos = int(raw_input())
-a= [int(x) for x in raw_input().split()]
-lista = list((int(i) for i in a))
-cantidad = len(lista)
-informacion = dict()
-fin = len(lista)-1
-for i in range (cantidad):
-    informacion[i] = lista[i]
-while (fin != -1):
-    pasar = -1
-    for i in range (0,fin):
-        if lista[i] > lista[i + 1]:
-            temporal = lista[i]
-            lista[i] = lista[i+1]
-            lista[i + 1] = temporal
-            pasar = i
-    fin = pasar
-respuesta = ''
-for i in lista:
-    for j in range (len(lista)):
-        if i == informacion[j]:
-            respuesta += str(j + 1) + ' '
-print(respuesta)
+#!/usr/bin/env python
+"""This script shows initial indexes of the array members after they are reordered by sorting."""
+DATOS = int(raw_input())
+A = [int(x) for x in raw_input().split()]
+LISTA = list((int(i) for i in A))
+CANTIDAD = len(LISTA)
+INFORMACION = dict()
+FIN = len(LISTA)-1
+for i in range(CANTIDAD):
+    INFORMACION[i] = LISTA[i]
+while FIN != -1:
+    PASAR = -1
+    for i in range(0, FIN):
+        if LISTA[i] > LISTA[i + 1]:
+            temporal = LISTA[i]
+            LISTA[i] = LISTA[i+1]
+            LISTA[i + 1] = temporal
+            PASAR = i
+    FIN = PASAR
+RESPUESTA = ''
+for i in LISTA:
+    for j in range(len(LISTA)):
+        if i == INFORMACION[j]:
+            RESPUESTA += str(j + 1) + ' '
+print RESPUESTA
