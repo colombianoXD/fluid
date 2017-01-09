e39c2c786d1c7e97001c49117cd9fce15e9e634e
Rafael Alvarez <ralvarez@fluid.la>
2017-01-08 22:28:40 -0500

    Depurar verificación ortografica en CI

--- check-solutions.sh
+++ check-solutions.sh
@@ -1,11 +1,13 @@
-#!/bin/bash
+#!/bin/bash -x
 
 FILES=$(find . -iname *.feature)
 
 # apt-get install cucumber
 echo "### Verificación sintactica"
-cucumber -f progress -q -m -s $FILES
-
+#cucumber -f progress -q -m -s $FILES
+cucumber $FILES
+RESULT=$?
+test $RESULT -ne 0 && exit 1 || exit 0
 
 # apt-get install aspell aspell-es
 echo ""
@@ -13,8 +15,11 @@ echo "### Verificación ortografica"
 WORDS=$(aspell -l es list < $FILES)
 if [ -z "$WORDS" ]; then
   echo "Sin errores de ortografia."
+  RESULT=0
 else
   echo "Con errores de ortografia."
   echo "$WORDS"
+  RESULT=1
 fi
 
+test $RESULT -ne 0 && exit 1 || exit 0
