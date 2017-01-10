cf8454ecace701abdc5591f1a79e74fd4ea9718d
pach092 <pachobernal5@gmail.com>
2017-01-09 20:01:24 -0500

    Update pach092.py

--- challenges/codeabbey/022/pach092.py
+++ challenges/codeabbey/022/pach092.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 """This script calculates minumum printing times
 for each of testcases, separated by spaces."""
-DATOS = int(raw_input())
+import math
+DATOS = int(input())
 RESPUESTA = []
 for hojas in range(DATOS):
     valores = map(int, raw_input().split())
-    x, y, n = int(valores[0]), int(valores[1]), int(valores[2])
-    impresora_1 = (y * n / (x + y))
-    impresora_2 = (x * n / (x + y))
-    respuesta1 = int(max((impresora_1 + 1) * x, impresora_2 * y))
-    respuesta2 = int(max(impresora_1 * x, (impresora_2 + 1) * y))
-    RESPUESTA.append(str(min(respuesta1, respuesta2)))
+    x, y, n = float(valores[0]), float(valores[1]), float(valores[2])
+    maximo = max(x, y)
+    minimo = min(x, y)
+    maximo_tiempo = math.ceil(n * minimo / (x + y)) * int(maximo)
+    minimo_tiempo = math.ceil(n * maximo / (x + y)) * int(minimo)
+    RESPUESTA.append(str(int(min(maximo_tiempo, minimo_tiempo))))
 print ' '.join(RESPUESTA)
