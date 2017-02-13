76f91869fa783ca2be13f365e7265770660350bd
pach092 <pachobernal5@gmail.com>
2017-02-13 17:09:19 -0500

    Create pach092.rb

---
+++ challenges/codeabbey/031/pach092.rb
@@ -0,0 +1,27 @@
+=begin
+This script rotates string by K characters
+=end
+cantidad = gets.chomp.to_i
+resultado = []
+cantidad.times do
+  (k, s) = gets.chomp.split
+  k = k.to_i
+  longitud = s.length
+  if k < 0
+    indice = (longitud - 1)*k.abs
+    j = 1
+    indice_final = 0
+  else
+    indice = 0
+    j = -1
+    indice_final = (longitud-1)*k
+  end
+  until indice == indice_final do
+    t = s[(indice+j) % longitud]
+    s[(indice+j) % longitud]=s[indice % longitud]
+    s[indice % longitud]=t
+    indice -= j
+  end
+  resultado << s
+end
+puts resultado.join(" ")
