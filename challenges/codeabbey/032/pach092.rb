fbae4442e1f153e5eb636587dcd5ee58c854032a
pach092 <pachobernal5@gmail.com>
2017-02-13 17:10:39 -0500

    Create pach092.rb

---
+++ challenges/codeabbey/032/pach092.rb
@@ -0,0 +1,28 @@
+=begin
+This script contain the number of person who will remain at the end. Initial
+numbering starts from 1.
+=end
+datos = gets
+datos = datos.split(" ").map(&:to_i)
+n = datos[0]
+k = datos[1]
+datos = []
+n.times do |i|
+  datos[i] = i + 1
+end
+i = 0
+contador = 1
+while datos.length != 1
+  if contador == k
+    datos.delete_at(i)
+    contador = 0
+    i -= 1
+  end
+  if i >= datos.length - 1
+    i = 0
+  else
+    i += 1
+  end
+  contador += 1
+end
+print datos[0].to_s
