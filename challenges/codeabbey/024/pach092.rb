1d6c874c382d04b14ec5dd0e67c2a5167cbd011d
pach092 <pachobernal5@gmail.com>
2017-02-13 21:48:37 -0500

    Update pach092.rb

--- challenges/codeabbey/024/pach092.rb
+++ challenges/codeabbey/024/pach092.rb
@@ -1,18 +1,14 @@
-=begin
-This script contains number of iterations for sequences with such initial
-values to come to the loop.
-=end
 cantidad = gets.chomp.to_i
 datos = gets.chomp.split.map(&:to_i)
 resultado = []
-for a in (0...cantidad)
+(0...cantidad).each do |a|
   contador = 0
   truncados = []
   numero = datos[a]
   until truncados.include? numero
     truncados << numero
     contador += 1
-    numero = (numero * numero) / 100 % 10000
+    numero = (numero * numero) / 100 % 10_000
   end
   resultado << contador
 end
