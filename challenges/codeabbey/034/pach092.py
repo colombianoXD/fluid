da93928c327efeee905ae8c47539031e413b755d
pach092 <pachobernal5@gmail.com>
2017-02-17 08:35:28 -0500

    Update pach092.py

--- challenges/codeabbey/034/pach092.py
+++ challenges/codeabbey/034/pach092.py
@@ -1,20 +1,20 @@
 '''This script contains solutions values of x which satisfy equation with
 given coefficents'''
 import math
-DATOS = int(raw_input())
-RESPUESTA = []
-for i in range(0, DATOS):
+DATA = int(raw_input())
+ANSWER = []
+for i in range(0, DATA):
     d = [float(x) for x in raw_input().split(" ")]
-    minimo = 0.0
-    maximo = 100.0
-    while (maximo - minimo) > pow(10, -7):
-        x = (maximo + minimo)/2
+    mini = 0.0
+    maxi = 100.0
+    while (maxi - mini) > pow(10, -7):
+        x = (maxi + mini)/2
         r = d[0] * x + d[1] * pow(x, 1.5) - d[2] * math.exp(-x / 50.0) - d[3]
         if r < 0:
-            minimo = x
+            mini = x
         if r > 0:
-            maximo = x
+            maxi = x
         if r == 0:
             break
-    RESPUESTA.append(str(x))
-print ' '.join(RESPUESTA)
+    ANSWER.append(str(x))
+print ' '.join(ANSWER)
