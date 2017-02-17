7fffd39e32f63a84e273597a371281a0f3c588b9
pach092 <pachobernal5@gmail.com>
2017-02-17 15:34:46 -0500

    Update pach092.py

--- challenges/codeabbey/049/pach092.py
+++ challenges/codeabbey/049/pach092.py
@@ -2,14 +2,14 @@
 (by value 1) or second (by value 2). There would be no draws."""
 AMOUNT = input()
 for i in range(int(AMOUNT)):
-    ar = raw_input().split()
-    v1, v2 = 0, 0
-    for j in ar:
+    DATA = raw_input().split()
+    game_1, game_2 = 0, 0
+    for j in DATA:
         if j in ('RS', 'SP', 'PR'):
-            v1 += 1
+            game_1 += 1
         if j in ('SR', 'PS', 'RP'):
-            v2 += 1
-    if v1 > v2:
+            game_2 += 1
+    if game_1 > game_2:
         print '1 '
     else:
         print '2 '
