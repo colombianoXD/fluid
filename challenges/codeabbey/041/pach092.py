7ea5cc76afe3993fba140817fbda6bf64107f557
pach092 <pachobernal5@gmail.com>
2017-02-17 08:39:05 -0500

    Update pach092.py

--- challenges/codeabbey/041/pach092.py
+++ challenges/codeabbey/041/pach092.py
@@ -1,7 +1,7 @@
 """This script contains selected medians of triplets, separated by spaces."""
-DATOS = int(raw_input())
-for minimo in range(DATOS):
-    valores = raw_input().split()
-    datos = int(valores[0]), int(valores[1]), int(valores[2])
-    orden = sorted(datos)
-    print orden[1], ''
+DATA = int(raw_input())
+for minimo in range(DATA):
+    value = raw_input().split()
+    data = int(value[0]), int(value[1]), int(value[2])
+    sort = sorted(data)
+    print sort[1], ''
