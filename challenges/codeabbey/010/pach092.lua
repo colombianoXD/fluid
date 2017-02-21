5c7ef724945ffb8aa4397e38cebabc6725db6eda
pach092 <pachobernal5@gmail.com>
2017-02-21 16:15:51 -0500

    Create pach092.lua

---
+++ challenges/codeabbey/010/pach092.lua
@@ -0,0 +1,9 @@
+amount = io.read("*n")
+answer = {}
+for i = 1, amount do
+    x1, y1 = io.read("*n", "*n")
+    x2, y2 = io.read("*n", "*n")
+    slope = math.floor((y2 - y1) / (x2 - x1))
+    interception = math.floor(y1 - slope * x1)
+    io.write("(", slope, " ", interception, ") ")
+end
