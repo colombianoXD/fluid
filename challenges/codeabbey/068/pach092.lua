77f70e109c8ea917ca81bec0fb274113d0c75bf8
pach092 <pachobernal5@gmail.com>
2017-02-27 14:57:55 -0500

    Update pach092.lua

--- challenges/codeabbey/068/pach092.lua
+++ challenges/codeabbey/068/pach092.lua
@@ -2,6 +2,6 @@ amount = io.read("*n")
 answer = {}
 for i = 1, amount do
     d, a, b = io.read("*n", "*n", "*n")
-    table.insert(answer, (a * ((d - 0) / (a + b))))
+    table.insert(answer, (a * (d / (a + b))))
 end
 io.write(table.concat(answer, " "))
