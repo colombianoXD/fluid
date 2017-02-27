0cb950d01951942cb8f9c59351cf8336d69da1ed
pach092 <pachobernal5@gmail.com>
2017-02-27 18:02:51 -0500

    Update pach092.lua

--- challenges/codeabbey/067/pach092.lua
+++ challenges/codeabbey/067/pach092.lua
@@ -2,15 +2,15 @@ amount = io.read("*n")
 for i = 1, amount do
     fibonacci = {0, 1, 1}
     number = io.read("*n")
-    a = 2
+    fibonacci_index = 2
     add = 0
     while add < number do
-        add = fibonacci[a] + fibonacci[a + 1]
+        add = fibonacci[fibonacci_index] + fibonacci[fibonacci_index + 1]
         table.insert(fibonacci, add)
-        a = a + 1
+        fibonacci_index = fibonacci_index + 1
     end
     if number == 0 then
-        a = 0
+        fibonacci_index = 0
     end
-    print(a)
+    print(fibonacci_index)
 end
